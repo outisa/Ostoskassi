32c3fab01abc930b630979f70933dcb2757f511c
outisa <savolainen.outi@gmail.com>
2019-03-28 13:14:39 +0200

    Ilmoitusten parantaminen kirjautumisen yhteydessä

--- application/auth/views.py
+++ application/auth/views.py
@@ -24,7 +24,7 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
-    return render_template("auth/index2.html")
+    return redirect(url_for("index2"))
 
 @app.route("/auth/newAccount", methods = ["GET", "POST"])
 def auth_create():
@@ -39,7 +39,7 @@ def auth_create():
     db.session().add(t)
     db.session().commit()
 
-    return render_template("auth/index2.html")
+    return redirect(url_for("index2"))
 
 @app.route("/auth/delete/<user_id>", methods = ["POST", "GET"])
 @login_required
@@ -49,4 +49,4 @@ def auth_delete(user_id):
    db.session().commit()
 
 
-   return redirect(url_for("index"))
+   return redirect(url_for("index3"))
--- application/views.py
+++ application/views.py
@@ -5,6 +5,10 @@ from application import app
 def index():
     return render_template("index.html")
 
-@app.route("/")
+@app.route("/logout")
 def index2():
     return render_template("index2.html")
+
+@app.route("/delete")
+def index3():
+    return render_template("index3.html")
